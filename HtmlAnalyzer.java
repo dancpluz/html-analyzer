0f8d8299b8a26d316b9e71ec595b20255a43b0b8
DESKTOP-SCBAE9E\Daniel <dan08jan@gmail.com>
2023-03-25 14:15:48 -0300

    Comentários

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,14 +1,16 @@
 package HtmlAnalyzer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Stack;
 
 public class HtmlAnalyzer {
-    public static void main(String[] args) {
-        String result = analyzeHTML("http://hiring.axreng.com/internship/example1.html");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+        String result = analyzeHTML(bufferReader.readLine());
         System.out.println(result);
     }
 
@@ -18,8 +20,8 @@ public class HtmlAnalyzer {
         URL url = new URL(urlStr);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-        connection.setConnectTimeout(2000); // Timeouts para evitar erros de conexão
-        connection.setReadTimeout(2000);
+        connection.setConnectTimeout(1000); // Timeouts para evitar erros de conexão
+        connection.setReadTimeout(1000);
 
         // BufferedReader para ler cada linha do código
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -32,40 +34,45 @@ public class HtmlAnalyzer {
         pastTags.push("html");
         TreeNode root = new TreeNode(reader.readLine());
         
+        // Loop em todas as linhas do html
         String line;
         while ((line = reader.readLine()) != null) {
             line = line.trim();
             int j = line.indexOf('<');
+            
+            // Caso a linha não tenha uma tag, adiciona com filho da raiz
             if (j == -1) {
                 root.children.add(new TreeNode(line));
                 continue;
             }
             
             String fullTag = line;
+            // Formata a tag se for de abertura ou fechamento
             boolean closeTag = fullTag.charAt(1) == '/';
             String tag = closeTag ? fullTag.substring(2,fullTag.length()-1) : fullTag.substring(1,fullTag.length()-1);      
             
-            if (closeTag) {
+            
+            if (!closeTag) {
+                pastTags.push(tag);
+                
+                // Caso a tag seja de abertura adiciona na pilha de árvores e cria uma nova raiz com a tag atual
+                stack.push(root);
+                root = new TreeNode(fullTag);
+            } else {
+                
                 if (!pastTags.peek().equals(tag)) return "malformed HTML";
-                
                 pastTags.pop();
-                
                 if (stack.isEmpty()) break;
                 
+                // Caso a tag seja de fechamento, adiciona a raiz atual como filha da árvore do topo e muda a raiz para ela
                 stack.peek().children.add(root);
                 root = stack.peek();
                 stack.pop();
-            } else {
-                pastTags.push(tag);
-                stack.push(root);
-                root = new TreeNode(fullTag);
             }
         }
-        
         reader.close();
         
         if (!pastTags.isEmpty()) return "malformed HTML";
-        
         return root.deepest().tagName;
         
     // Caso não seja possível conectar
